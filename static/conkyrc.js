235e753d2bb406ef41cdad091231e468a5b60bb0
Trux <christophe-lyraud@gmail.com>
2012-06-18 10:28:37 +0200

    modification selecteur des inputs

--- static/conkyrc.js
+++ static/conkyrc.js
@@ -10,17 +10,17 @@ $(document).ready(function()
 		compteur = compteur+1;
 		
 
-		var tabInput = document.getElementsByTagName("input");
+		var tabInput = $("#form1 :input");
  		var n = tabInput.length;
  	//	$("#result").html('<div id='+compteur+'></div>');
 
-	 	for (i=0; i<n-1; i++)
+	 	for (i=0; i<n-2; i++)
 
  		{
  		
  		name = tabInput[i].getAttribute('name');
  		value = tabInput[i].value;
- 		alert(value);
+ 		
  	  	if (value != "")
   		{
 
@@ -34,8 +34,25 @@ $(document).ready(function()
 
 	 	}
 	 	
-	 	$("#form"+parseInt(compteur)+"").css("display, none");
-	 	$("#form"+(parseInt(compteur)+1)+"").css("visibility, visible");
+	 	$("#form"+parseInt(compteur)+"").css("display", "none");
+	 	$("#form"+(parseInt(compteur)+1)+"").css("display", "block");
+	 	$("#previous").css("display", "block");
+
+
+
+	});
+
+	$("#previous").click(function() {
+		$("#result").empty();
+		$("#previous").css("display", "none");			 	
+	 	$("#form"+parseInt(compteur)+"").css("display", "block");
+	 	$("#form"+(parseInt(compteur)+1)+"").css("display", "none");
+	 	compteur = compteur-1;
+	});
+
+	$("#next2").click(function() {
+		compteur = compteur+1;
+		
 
 
 
