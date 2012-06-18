af17c18d00fe177baba591a90e644dc4193bdec5
Trux <christophe-lyraud@gmail.com>
2012-06-18 14:27:10 +0200

    correction probleme ajout checbox

--- static/conkyrc.js
+++ static/conkyrc.js
@@ -6,14 +6,12 @@ $(document).ready(function()
 
 {
 
-	$("#next").click(function() {
+	$("#next1").live("click", function(event){
 		compteur = compteur+1;
 		
-
 		var tabInput = $("#form1 :input");
  		var n = tabInput.length;
- 	//	$("#result").html('<div id='+compteur+'></div>');
-
+ 	
 	 	for (i=0; i<n-2; i++)
 
  		{
@@ -37,9 +35,8 @@ $(document).ready(function()
 	 	$("#form"+parseInt(compteur)+"").css("display", "none");
 	 	$("#form"+(parseInt(compteur)+1)+"").css("display", "block");
 	 	$("#previous").css("display", "block");
-
-
-
+	 	$("#next1").attr("id","next2");
+	 	$("#result").append('TEXT\n');  
 	});
 
 	$("#previous").click(function() {
@@ -48,10 +45,27 @@ $(document).ready(function()
 	 	$("#form"+parseInt(compteur)+"").css("display", "block");
 	 	$("#form"+(parseInt(compteur)+1)+"").css("display", "none");
 	 	compteur = compteur-1;
+	 	$("#next2").attr("id","next1");
 	});
 
-	$("#next2").click(function() {
+
+
+	$("#next2").live("click", function(event){
+		
 		compteur = compteur+1;
+		var tabInput = $("input:checked[name=options]");
+		
+		var n = tabInput.length;
+	 	for (i=0; i<n; i++)
+	 	
+ 		{
+ 		value = String(tabInput[i].value);
+ 		alert(value);
+ 		
+ 		$("#result").append(value+'\n');	
+
+	 	}
+		
 		
 
 
@@ -61,3 +75,6 @@ $(document).ready(function()
 
 
 });
+
+
+	
