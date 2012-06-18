9abca222dae9a14292a71e441ede05cb495d81cd
Trux <christophe-lyraud@gmail.com>
2012-06-18 03:59:57 +0200

    ajout templates

--- conkystador.py
+++ conkystador.py
@@ -1,14 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template
+from flaskext.wtf import Form, TextField, Required
+
+
 app = Flask(__name__)
 
 @app.route('/')
 def conkystador():
-    return 'Hasta la victoria siempre!'
+	return render_template('conkystador.html', name='wtf is that name')
+	 
+    
 
 
-@app.route('/about')
+
+@app.route('/42')
 def about():
-    return 'so long and thanks for all the fish'
+    return render_template('42.html')
 
 if __name__ == '__main__':
-    app.run()
+    
+    app.run(debug=True)
